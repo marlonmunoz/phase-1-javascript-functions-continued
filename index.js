75735201a7f9ac580400e9df243ddefa57592a98
Marlon Munoz <marlon@marlonshade.com>
2024-07-16 17:29:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(newActivity = "roller-skate") {
+    return `This Saturday, I want to ${newActivity}!`
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+
+function wrapAdjective(flair = "special") {
+    return function(adjective = "*") {
+        return `You are ${flair}${adjective}${flair}!`;
+};
+}
+
+const encouragingPromptFunction = wrapAdjective("*");
+console.log(encouragingPromptFunction("amazing")); 
